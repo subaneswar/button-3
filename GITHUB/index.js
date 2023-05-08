6fad6e8debadc78e9dfe515b8807a0e61f7924e0
subaneswar <132871334+subaneswar@users.noreply.github.com>
2023-05-08 08:44:32 -0700

    Add files via upload

---
+++ GITHUB/index.js
@@ -0,0 +1,14 @@
+function color(){
+    document.body.style.background="orange"
+}
+function color1(){
+    document.body.style.background="white"
+}
+function hover(){
+    document.querySelector("h1").style.background="red"
+    document.querySelector("h1").style.color="aqua"
+}
+function hover1(){
+    document.querySelector("h1").style.background="white"
+    document.querySelector("h1").style.color="blue"
+}
